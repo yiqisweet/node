3940428a30a71f608f386cb113edb619a2159923
yiqisweet <mynameislxmax@outlook.com>
2017-07-02 13:09:11 +0800

    update

--- commonjs/main.js
+++ commonjs/main.js
@@ -1,9 +1,1 @@
 
-console.log(require('./sub').count += 1);
-console.log(require('./sub').count += 1);
-console.log(require('./sub').count += 1);
-
-Object.keys(require.cache).forEach(function(x){
-    delete require.cache[x]
-})
-console.log(require('./sub').count += 1);
