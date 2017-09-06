a1af12e527d1500e93a43eb8583f257fd9f3583c
Randy Lee <mynameislxmax@outlook.com>
2017-09-06 17:31:15 +0800

    Update template.js

--- ES6/template.js
+++ ES6/template.js
@@ -1,26 +1,26 @@
-var name = '张三',
-    age = 18,
-    city = '北京'
+//var name = '张三',
+   // age = 18,
+  //  city = '北京'
 
-console.log(`我叫${name},今年${age}岁,住在${city}`)
+//console.log(`我叫${name},今年${age}岁,住在${city}`)
 
 
-var obj = {
-  name : '李四',
-  age : 28
-}
+//var obj = {
+ // name : '李四',
+ // age : 28
+//}
 
-console.log(`${obj.name},${obj.age}`)
+//console.log(`${obj.name},${obj.age}`)
 
-var x = 10,
-    y = 20;
+//var x = 10,
+ //   y = 20;
 
-console.log(`${x} + ${y} = ${ x + y }`)
-console.log(`${x} * ${y}  = ${x * y} `)
-console.log(`${(x+y) * x}`)
+//console.log(`${x} + ${y} = ${ x + y }`)
+//console.log(`${x} * ${y}  = ${x * y} `)
+//console.log(`${(x+y) * x}`)
 
-function num(){
-  return 1 + 2;
-}
+//function num(){
+//  return 1 + 2;
+//}
 
-console.log(`${num()}`)
+//console.log(`${num()}`)
