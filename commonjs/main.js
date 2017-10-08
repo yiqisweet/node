ae98884c0c39f1ceb31119a3bed1b4679451faff
Randy Lee <mynameislxmax@outlook.com>
2017-10-08 10:57:38 +0800

    Update main.js

--- commonjs/main.js
+++ commonjs/main.js
@@ -3,6 +3,7 @@ console.log(require('./sub').count += 1);
 console.log(require('./sub').count += 1);
 console.log(require('./sub').count += 1);
 
+//清除所有模块缓存
 Object.keys(require.cache).forEach(function(x){
     delete require.cache[x]
 })
