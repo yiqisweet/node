c192639cac3c61765b9e26da779d0c2843dfce5c
yiqisweet <mynameislxmax@outlook.com>
2017-10-31 14:16:11 +0800

    branch dev test

--- code/index.js
+++ code/index.js
@@ -3,8 +3,10 @@
  * @Date:   2017-08-20 08:43:51
  * @Email:  mynameislxmax@outlook.com
  * @Filename: index.js
- * @Last modified by:   Randy
- * @Last modified time: 2017-08-20 08:43:52
+ * @Last modified by:   randy
+ * @Last modified time: 2017-10-31 02:15:16
  */
 
 //测试
+
+console.log(1);
